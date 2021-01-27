cabc2819c141767ec708d7f286233642a0ef7507
Sohaib Furqan <sohaibfurqan@hotmail.com>
2021-01-27 22:02:53 +0500

    Added mediator pattern example

---
+++ mediator pattern/app.js
@@ -0,0 +1,50 @@
+// chatroom is the mediator, users are the colleagues of the chatroom
+
+const User=function(name){
+  this.name =name;
+  this.chatroom =null
+};
+
+User.prototype.send = function(message,to){
+  chatroom.send(message, this, to);
+}
+
+User.prototype.receive= function(message,from){
+  console.log(`${from.name} to ${this.name}: ${message}`);
+}
+
+const Chatroom = function(){
+  const users = {};
+
+  return{
+    register:function(user){
+      users[user.name] = user;
+      user.chatroom = this;
+    },
+
+    send: function(message,from,to){
+      if(to){
+        to.receive(message,from);
+      } else{
+        for (const key in users) {
+          if(users[key]!==from){
+            users[key].receive(message,from);
+          }
+          }
+        }
+      }
+    }
+  }
+
+  const chatroom = new Chatroom();
+
+  const brad = new User('brad'); 
+  const jeff = new User('jeff');
+  const sara = new User('sara');
+
+  chatroom.register(brad);
+  chatroom.register(jeff);
+  chatroom.register(sara);
+
+  brad.send("Hello Jeff",jeff);
+  jeff.send("Hello Everyone!");
