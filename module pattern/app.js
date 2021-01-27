5e3f86f5ea1e8c2918e59a6bf885e0fa748d4c75
Sohaib Furqan <sohaibfurqan@hotmail.com>
2021-01-27 12:37:12 +0500

    committed app.js

--- module pattern/app.js
+++ module pattern/app.js
@@ -1,19 +1,42 @@
 // Module Pattern
 
-const UIctrl = (function(){
-  const text = 'Hello World!';
+// const UICtrl = (function(){
+//   const text = 'Hello World';
 
-  const changeText = function(){
-    const element = document.querySelector('h1');
-    element.textContent = text;
-  };
+//   const changeText = function(){
+//     const element = document.querySelector('h1');
+//     element.textContent = text;
+    
+//   }
 
-  return {
-    callChangeText: function(){
-      changeText();
-      console.log(text);
-    }
+//   return{
+//     callChangeText: function(){
+//       changeText();
+//       console.log(text);
+//     }
+//   }
+// })();
+
+// UICtrl.callChangeText();
+
+// Revealing Module Pattern
+const itemCtrl = (function(){
+  const data =[];
+
+  function add(item){
+    data.push(item);
+    console.log("Item Added...");
+  }
+
+  function get(id){
+    return data.find(item=>item.id===id);
+  }
+
+  return{
+    add:add,
+    get:get
   }
 })();
 
-UIctrl.callChangeText();
+itemCtrl.add({id:1, name:"John"});
+console.log(itemCtrl.get(1));
